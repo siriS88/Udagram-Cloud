1fe4d53e86ce9d206a788749c18128574e8f0f00
Siri Singamneni <ssingamneni78@gmail.com>
2019-08-03 11:41:19 -0700

    Local server to filter images

---
+++ image-filter-server/src/server.ts
@@ -0,0 +1,51 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import { filterImageFromURL, deleteLocalFiles } from './util/util';
+import { runInNewContext } from 'vm';
+
+(async () => {
+
+  // Init the Express application
+  const app = express();
+
+  // Set the network port
+  const port = process.env.PORT || 8082;
+
+  // Use the body parser middleware for post requests
+  app.use(bodyParser.json());
+
+  // Root Endpoint
+  // Displays a simple message to the user
+  app.get("/", async (req, res) => {
+    res.send("try GET /filteredimage?image_url={{}}")
+  });
+
+  app.get("/filteredimage", async (req, res) => {
+
+    const { image_url } = req.query;
+    if (!image_url) {
+      return res.status(400)
+        .send(`image_url is required`);
+    } else {
+      const filteredImagePath = await filterImageFromURL(image_url);
+      console.log(filteredImagePath);
+      res.sendFile(filteredImagePath, async (err) => {
+        if (err) {
+          console.log(err);
+        } else {
+          await deleteLocalFiles([filteredImagePath]);
+
+        }
+      })
+
+    }
+
+  });
+
+
+  // Start the Server
+  app.listen(port, () => {
+    console.log(`server running http://localhost:${port}`);
+    console.log(`press CTRL+C to stop server`);
+  });
+})();
